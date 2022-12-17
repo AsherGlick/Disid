8f7a0777edbd6733d77256629f3df869b7197df8
Asher Glick <asher.glick@gmail.com>
2022-12-17 11:35:39 -0600

    adding a script to run all lints and test

---
+++ presubmit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+source ./venv/bin/activate
+FILES=`find . -type f -name "*.py" -not -path "./venv/*"
+flake8 --ignore=E501,E266,W503 $FILES
+mypy --strict $FILES
+
+
+# Run Unit Tests
+coverage run -m unittest discover -s . -p '*_test.py'
+
+# Integration Tests
+# coverage run -a someotherfile.py args
+
+coverage report -m --omit='venv/*'
