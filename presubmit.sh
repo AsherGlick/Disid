fa205abf82263661e0c7ad0202e4da018d01aabe
Asher Glick <asher.glick@gmail.com>
2024-01-06 05:44:02 -0600

    fixing presubmit script

--- presubmit.sh
+++ presubmit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./venv/bin/activate
-FILES=`find . -type f -name "*.py" -not -path "./venv/*"
+FILES=`find . -type f -name "*.py" -not -path "./venv/*"`
 flake8 --ignore=E501,E266,W503 $FILES
 mypy --strict $FILES
 
